a243501fd7bd5ac7ccb0e77010d613497df8d62b
DescriCS <descrirg@gmail.com>
2017-06-21 12:21:49 +0200

    Update inscription connexion deconnexion

---
+++ connexion.php
@@ -0,0 +1,12 @@
+<?php
+include('inc/pdo.php');
+include('inc/functions.php');
+
+$title = 'Page de connexion';
+
+?>
+
+<?php include('inc/header.php'); ?>
+
+
+<?php include('inc/footer.php'); ?>
---
+++ deconnexion.php
@@ -0,0 +1,12 @@
+<?php
+include('inc/pdo.php');
+include('inc/functions.php');
+
+$title = 'Page de deconnexion';
+
+?>
+
+<?php include('inc/header.php'); ?>
+
+
+<?php include('inc/footer.php'); ?>
