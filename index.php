447f3d1e58264ddb99cc69e348319d5abf1b91dc
fred <fred_fahrer@hotmail.com>
2017-06-21 14:09:37 +0200

    first

--- index.php
+++ index.php
@@ -4,8 +4,28 @@ include('inc/functions.php');
 
 $title = 'Page d\'acceuil';
 ?>
-test ici
-
-zaezaezaezae
 <?php include('inc/header.php'); ?>
+
+<?php
+
+  $sql = "SELECT * FROM movies_full ORDER BY RAND() DESC LIMIT 100";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $movies_full = $query->fetchAll();
+
+
+
+ foreach ($movies_full as $movie) { ?>
+   <div class="movie">
+      <a href="detail.php?id=<?php echo $movie['id']; ?>">
+        <img src="./asset/posters/<?php echo $movie['id']; ?>.jpg" alt="<?php echo $movie['title']; ?>">
+      </a>
+   </div>
+ <?php } ?>
+
+
+
+
+
+
 <?php include('inc/footer.php'); ?>
