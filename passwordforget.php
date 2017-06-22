cb76883d595399c9b03db3bbc4c065d842e5b47b
DescriCS <descrirg@gmail.com>
2017-06-22 11:39:38 +0200

    Création de password forget et password modification mais pas fini

---
+++ passwordforget.php
@@ -0,0 +1,62 @@
+<?php
+include('inc/pdo.php');
+include('inc/functions.php');
+
+if(!empty($_POST['submitbtn'])){
+	// protection XSS
+	$email = trim(strip_tags($_POST['email']));
+
+		if(empty($email) || (filter_var($email, FILTER_VALIDATE_EMAIL)) === false) {
+	      $error['email'] = 'Adresse email invalide.';
+	    }
+	    elseif(strlen($email) > 155) {
+	      $error['email'] = 'Votre adresse e-mail est trop longue.';
+	    }
+	    // verifier que email exist dans la BDD
+	    else {
+	    	$sqlmail = "SELECT pseudo,email,token FROM users WHERE email = :email";
+                $smtp = $pdo->prepare($sqlmail);
+                $smtp->bindValue(':email',$email);
+                $smtp->execute();
+                $user = $smtp->fetch();
+
+                if(!$user) {
+                   $error['email'] = 'Cette adresse e-mail n\'est pas enregistrer sur notre site.';
+                } else {
+                	// envoi email avec lien contenant le token
+                	$tokenprepare = urlencode($user['token']);
+
+                	// mettre une date d'expiration de validation du token ++ securite ++
+                	$emailprepare = urlencode($user['email']);
+                	// envoie de mail par
+                	$body = '<p>Veuillez cliquer sur le lien ci-dessous</p>';
+                	$body .= '<p><a href="http://localhost/J15/movies-projet/passwordmodification.php?email='.$emailprepare.'&token='.$tokenprepare.'">ICI</a></p>';
+                	$body .= '<p>Rappel : Votre pseudo => '. $user['pseudo'] .'</p>';
+                	echo $body;
+                  // ici faire envoie email ++++++
+                  // Avec PHPMAILER
+                }
+	    }
+}
+
+?>
+
+<?php include('inc/header.php'); ?>
+
+<h4>Récupération de mot de passe</h4>
+
+<form class="" action="" method="post">
+
+  <!-- Login -->
+  <div class="email">
+    <span class="error"><?php if(!empty($error['email'])) { echo $error['email']; } ?></span>
+    <input type="text" placeholder="Pseudo/E-mail" name="email" class="email" value="<?php if(!empty($_POST['email'])) { echo $_POST['email']; } ?>" />
+  </div>
+
+  <br>
+
+  <input type="submit" name="submitbtn" class="btn" value="Envoyer">
+
+</form>
+
+<?php include('inc/footer.php'); ?>
---
+++ passwordmodification.php
@@ -0,0 +1,51 @@
+<?php
+include('inc/pdo.php');
+include('inc/functions.php');
+
+$error   = array();
+$success = false;
+$title   = 'Modification de mot de passe';
+
+if (!empty($_POST['submitnewpassword'])) {
+
+  $password1  = trim(strip_tags($_POST['newpassword']));
+  $password2  = trim(strip_tags($_POST['newpassword2']));
+
+  if ($password1 != $password2) {
+    $error['password'] = 'Les mots de passe ne sont pas identiques.';
+  } elseif(strlen($password1) <= 5) {
+    $error['password'] = 'Votre mot de passe doit faire plus de caractères.';
+  }
+
+  if(count($error) == 0) {
+    $success = true;
+    // hash password
+    $passwordhash  = password_hash($password1, PASSWORD_DEFAULT);
+
+    $sql = "UPDATE users (pseudo,email,password,created_at,updated_at,role,token) VALUES ( :pseudo, :email, :password, NOW(), NOW(),'Membre', :token)";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':password',$passwordhash, PDO::PARAM_STR);
+    $query->execute();
+
+    echo 'Votre mot de passe a bien été changer';
+  }
+}
+
+?>
+
+<?php include('inc/header.php'); ?>
+
+<h1>Modifier votre mot de passe</h1>
+
+<form method="post">
+
+  <label for="newpassword">Votre nouveau mot de passe :</label>
+  <input type="password" name="newpassword" value="">
+
+  <label for="newpassword2">Confirmer votre nouveau mot de passe :</label>
+  <input type="password" name="newpassword2" value="">
+
+  <input type="submit" name="submitnewpassword" value="Modifier">
+</form>
+
+<?php include('inc/footer.php'); ?>
